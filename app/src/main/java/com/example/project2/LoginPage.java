18182a95c6baea47a04d290a0e30b774ded61e97
Alex Phengsavath <aphengsavath@csumb.edu>
2023-05-12 15:41:19 -0700

    please work

---
+++ app/src/main/java/com/example/project2/LoginPage.java
@@ -0,0 +1,19 @@
+package com.example.project2;
+
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginPage extends AppCompatActivity {
+    private TextView mLoginText;
+
+    private EditText mUsernameText;
+    private EditText mPasswordText;
+
+    private Button mLoginButton;
+
+    //test to see if Git updates
+
+}
