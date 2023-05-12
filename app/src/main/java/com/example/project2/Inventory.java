02d3fa94904938c6aa866029bbc97801aeb3e7bf
kevin <kbradshaw@csumb.edu>
2023-05-12 16:34:01 -0700

    Implemented some inventory stuff, main activity xml, and fields of MainActivity

--- app/src/main/java/com/example/project2/Inventory.java
+++ app/src/main/java/com/example/project2/Inventory.java
@@ -1,4 +1,87 @@
 package com.example.project2;
 
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.util.Date;
+
 public class Inventory {
+    @PrimaryKey(autoGenerate = true)
+    private int mInventoryID;
+
+    private String mName;
+    private double mValue;
+    private int mInventory;
+
+    private Date mDate;
+
+    private int mUserID;
+
+    public Inventory(String name, double value, int inventory, int userID) {
+        mName = name;
+        mValue = value;
+        mInventory = inventory;
+
+        mDate = new Date();
+
+        mUserID = userID;
+    }
+
+    public int getInventoryID() {
+        return mInventoryID;
+    }
+
+    public void setInventoryID(int inventoryID) {
+        mInventoryID = inventoryID;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+
+    public double getValue() {
+        return mValue;
+    }
+
+    public void setValue(double value) {
+        mValue = value;
+    }
+
+    public int getInventory() {
+        return mInventory;
+    }
+
+    public void setInventory(int inventory) {
+        mInventory = inventory;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public int getUserID() {
+        return mUserID;
+    }
+
+    public void setUserID(int userID) {
+        mUserID = userID;
+    }
+
+    @Override
+    public String toString() {
+        return "Stock #: " + mInventoryID + "\n" +
+                "Name: " + mName + "\n" +
+                "Price: $" + mValue + "\n" +
+                "User #: " + mUserID + "\n" +
+                "=-=-=-=-=-=-\n";
+    }
+
 }
--- app/src/main/java/com/example/project2/MainActivity.java
+++ app/src/main/java/com/example/project2/MainActivity.java
@@ -2,9 +2,30 @@ package com.example.project2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.project2.DB.InventoryDAO;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String USER_ID_KEY = "com.example.project2.userIDKey";
+    private static final  String PREFERENCES_KEY = "com.example.project2.PREFERENCES_KEY";
+
+    private TextView mMainMenuTitle;
+
+    private Button mShopButton;
+    private Button mOrderHistoryButton;
+    private Button mAdminButton;
+
+    private InventoryDAO mStockDAO;
+
+    private int mUserID = -1;
+
+    private SharedPreferences mPreferences = null;
+    private User mUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
