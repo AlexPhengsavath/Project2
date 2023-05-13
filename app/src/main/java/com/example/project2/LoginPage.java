6482812bbd2ca66ba47f9e931263fca1c2c4a916
Alex Phengsavath <aphengsavath@csumb.edu>
2023-05-12 17:09:04 -0700

    Added register button to activity_login.xml and added more to LoginPage.java

--- app/src/main/java/com/example/project2/LoginPage.java
+++ app/src/main/java/com/example/project2/LoginPage.java
@@ -1,10 +1,13 @@
 package com.example.project2;
 
+import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.project2.DB.InventoryDAO;
 
 public class LoginPage extends AppCompatActivity {
     private TextView mLoginText;
@@ -14,8 +17,33 @@ public class LoginPage extends AppCompatActivity {
 
     private Button mLoginButton;
 
-    //TODO: Create register button on activityLogin.xml and create variable here
+    private Bundle mRegisterButton;
 
+    private InventoryDAO mInventoryDAO;
+    
+    private String mUsernameString;
+    
+    private String mPasswordString;
+    
+    private User mUser;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        
+        wireupDisplay();
+        getDatabase();
+        
+    }
 
+    private void getDatabase() {
+    }
+
+    private void wireupDisplay() {
+        mLoginText = findViewById(R.id.textViewLoginText);
+
+        mUsernameText = findViewById(R.id.editText_Username);
+    }
 
 }
