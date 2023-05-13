3d9f9948050fc7342b662677000dfd4529feedea
Alex Phengsavath <aphengsavath@csumb.edu>
2023-05-12 19:32:17 -0700

    Started on CartActivity.java

--- app/src/main/java/com/example/project2/CartActivity.java
+++ app/src/main/java/com/example/project2/CartActivity.java
@@ -1,4 +1,17 @@
 package com.example.project2;
 
-public class CartActivity {
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CartActivity extends AppCompatActivity {
+    private static final String USER_ID_KEY = "com.example.project2.userIdKey";
+
+    private TextView mCartText;
+
+    private Button mPurchase;
+
+    private Button mRemove;
+
 }
--- app/src/main/java/com/example/project2/OrderActivity.java
+++ app/src/main/java/com/example/project2/OrderActivity.java
@@ -3,13 +3,10 @@ package com.example.project2;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.room.Room;
