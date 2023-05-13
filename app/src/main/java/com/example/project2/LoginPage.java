b005d3d1b33f2530a45d1fc410aab6c913130cc5
Alex Phengsavath <aphengsavath@csumb.edu>
2023-05-12 17:22:52 -0700

    added more to LoginPage.java

--- app/src/main/java/com/example/project2/LoginPage.java
+++ app/src/main/java/com/example/project2/LoginPage.java
@@ -1,9 +1,12 @@
 package com.example.project2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,7 +20,7 @@ public class LoginPage extends AppCompatActivity {
 
     private Button mLoginButton;
 
-    private Bundle mRegisterButton;
+    private Button mRegisterButton;
 
     private InventoryDAO mInventoryDAO;
     
@@ -44,6 +47,50 @@ public class LoginPage extends AppCompatActivity {
         mLoginText = findViewById(R.id.textViewLoginText);
 
         mUsernameText = findViewById(R.id.editText_Username);
+
+        mPasswordText = findViewById(R.id.editText_Password);
+
+        mLoginButton = findViewById(R.id.buttonLogin);
+
+        mRegisterButton = findViewById(R.id.buttonRegister);
+
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getValuesFromDisplay();
+                if(checkForUserInDatabase()){
+                    if(!validatePassword()){
+                        Toast.makeText(LoginPage.this, "Invalid password", Toast.LENGTH_SHORT).show();
+                    }else{
+                        Intent intent = MainActivity.intentFactory(this, mUser.getUserID());
+                        startActivity(intent);
+                    }
+                }
+            }
+        });
+
+        mRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getValuesFromDisplay();
+                if(checkForUserInDatabase() == false){
+                    User user = new User(mUsernameString, mPasswordString);
+                    mInventoryDAO.insert(user);
+
+                    Intent intent = MainActivity.intentFactory(this, mUser.getUserID());
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+
+    private boolean validatePassword() {
+    }
+
+    private boolean checkForUserInDatabase() {
+    }
+
+    private void getValuesFromDisplay() {
     }
 
 }
